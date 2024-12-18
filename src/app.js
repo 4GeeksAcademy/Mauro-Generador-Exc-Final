37cc0744f13a6ed16428f2e67d557d12a9b9709c
MauroG07 <71202727+MauroG07@users.noreply.github.com>
2024-12-18 20:15:28 +0000

    Lineamiento-de-Codigo-Exc-Gen-Mauro

--- src/app.js
+++ src/app.js
@@ -5,29 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = ExcuseGenerator;
-//write your code here
-let who = ["mi hermana pequeña,", "mi sobrina,", "mi Madre,", "mi Abuela,"];
-let action = [
-  "se enfermó y tuve que hacerme cargo de su cuidado,",
-  "se extravió en la ciudad y estuvo toda la noche fuera,",
-  "tuvo que ser hospitalizada,",
-  "perdió la cabeza y se dió a la fuga, tuve que salir a buscarla,"
-];
-let apologise = [
-  "lo siento mucho, lo tendré listo al finalizar el día.",
-  "pido mil disculpas, se lo entregaré hecho al acabar la jornada.",
-  "lo lamento, trataré de resolverlo en un momento.",
-  "por ello disculpeme, me ocuparé de inmediato."
-];
-let when = ["anoche,", "ayer por la noche,", "el viernes pasado por la noche,"];
-function ExcuseGenerator() {
+window.onload = function(){
+  excuseGenerator();
+} 
+
+function excuseGenerator() {
+  let who = ["mi hermana pequeña,", "mi sobrina,", "mi Madre,", "mi Abuela,"];
+  let action = [
+    "se enfermó y tuve que hacerme cargo de su cuidado,",
+    "se extravió en la ciudad y estuvo toda la noche fuera,",
+    "tuvo que ser hospitalizada,",
+    "perdió la cabeza y se dió a la fuga, tuve que salir a buscarla,"
+  ];
+  let apology = [
+    "lo siento mucho, lo tendré listo al finalizar el día.",
+    "pido mil disculpas, se lo entregaré hecho al acabar la jornada.",
+    "lo lamento, trataré de resolverlo en un momento.",
+    "por ello disculpeme, me ocuparé de inmediato."
+  ];
+  let when = ["anoche,", "ayer por la noche,", "el viernes pasado por la noche,"];
+  
   let randomWho = Math.floor(Math.random() * who.length);
   let randomAction = Math.floor(Math.random() * action.length);
-  let randomApologise = Math.floor(Math.random() * apologise.length);
+  let randomApology = Math.floor(Math.random() * apology.length);
   let randomWhen = Math.floor(Math.random() * when.length);
 
-  let ExcusaCompleta =
+  let excusaCompleta =
     "No he podido por que," +
     " " +
     when[randomWhen] +
@@ -36,11 +39,11 @@ function ExcuseGenerator() {
     " " +
     action[randomAction] +
     " " +
-    apologise[randomApologise];
+    apology[randomApology];
 
-  document.querySelector("#completeExc").innerHTML = ExcusaCompleta;
+  document.querySelector("#complete-excuse").innerHTML = excusaCompleta;
 }
 
-document.getElementById("refreshButton").addEventListener("click", function() {
-  ExcuseGenerator();
+document.getElementById("refresh-button").addEventListener("click", function() {
+  excuseGenerator();
 });
